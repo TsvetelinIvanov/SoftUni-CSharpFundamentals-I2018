11dad00222acaa319ccf27447039b195ad4531ef
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 16:15:15 +0200

    Create 02SumNumbers.cs

---
+++ CSharpAdvanced/04FunctionalProgrammingLab/02SumNumbers.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+namespace _02SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, int> parser = n => int.Parse(n);
+            int[] numbers = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
