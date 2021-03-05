9396a1ca052b88fe226f05eb40b37478cc257e86
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 09:06:57 +0200

    Create 06TrafficLight.cs

---
+++ CSharpAdvanced/01StacksAndQueuesLab/06TrafficLight.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace _06TrafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            int count = 0;
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
