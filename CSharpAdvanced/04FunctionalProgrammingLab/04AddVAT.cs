3dfa3abad3a2cbe641ace76670058ce8a9a2270a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 16:20:48 +0200

    Create 04AddVAT.cs

---
+++ CSharpAdvanced/04FunctionalProgrammingLab/04AddVAT.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+namespace _04AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).Select(n => n * 1.2).ToList()
+                .ForEach(n => Console.WriteLine($"{n:n2}"));
+        }
+    }
+}
