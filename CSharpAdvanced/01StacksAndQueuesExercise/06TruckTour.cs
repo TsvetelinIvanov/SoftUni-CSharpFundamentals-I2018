4103d14e687927062f67e88d66955e46706f1826
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 09:48:57 +0200

    Create 06TruckTour.cs

---
+++ CSharpAdvanced/01StacksAndQueuesExercise/06TruckTour.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int[]> queue = new Queue<int[]>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] pumpData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                queue.Enqueue(pumpData);
+            }
+
+            for (int startPump = 0; startPump < n; startPump++)
+            {
+                int fuel = 0;
+                bool isRounded = true;
+                for (int pump = 0; pump < n; pump++)
+                {
+                    int[] currentPump = queue.Dequeue();
+                    int pumpFuel = currentPump[0];
+                    int nextPumpDistance = currentPump[1];
+                    queue.Enqueue(currentPump);
+                    fuel += pumpFuel - nextPumpDistance;
+                    if (fuel < 0)
+                    {
+                        startPump += pump;
+                        isRounded = false;
+                        break;
+                    }
+                }
+
+                if (isRounded)
+                {
+                    Console.WriteLine(startPump);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
