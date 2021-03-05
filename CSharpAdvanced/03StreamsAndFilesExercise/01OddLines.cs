8ede7eb13ac1be221175ca12902df8e9cfe43042
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 15:30:44 +0200

    Create 01OddLines.cs

---
+++ CSharpAdvanced/03StreamsAndFilesExercise/01OddLines.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+
+namespace _01OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Resources/text.txt"))
+            {
+                int lineNumber = 0;
+                string line = string.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (lineNumber % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
