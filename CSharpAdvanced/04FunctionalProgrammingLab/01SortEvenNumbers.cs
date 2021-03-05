17b46fe14be618e5f852f62cc065e339a39ef5ad
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 16:13:03 +0200

    Create 01SortEvenNumbers.cs

---
+++ CSharpAdvanced/04FunctionalProgrammingLab/01SortEvenNumbers.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+namespace _01SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n))
+                .Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+            string result = string.Join(", ", numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
