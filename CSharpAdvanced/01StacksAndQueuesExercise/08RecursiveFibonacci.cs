8bc9e386238c09bf95d1d488382723a275f52bfd
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 10:26:23 +0200

    Create 08RecursiveFibonacci.cs

---
+++ CSharpAdvanced/01StacksAndQueuesExercise/08RecursiveFibonacci.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace _08RecursiveFibonacci
+{
+    class Program
+    {
+        static long[] numbers;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            numbers = new long[n + 2];
+            numbers[1] = 1;
+            numbers[2] = 1;
+            long fibonacciNumber = Fibonacci(n);
+            Console.WriteLine(fibonacciNumber);
+        }
+
+        static long Fibonacci(int n)
+        {
+            if (numbers[n] == 0)
+            {
+                numbers[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+
+            return numbers[n];
+        }
+    }
+}
