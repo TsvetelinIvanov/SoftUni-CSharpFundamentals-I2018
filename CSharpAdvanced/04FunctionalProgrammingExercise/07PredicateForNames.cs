03ba938bd5a25a0fa707b6a0b6a5c1de8dc19845
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 16:48:22 +0200

    Create 07PredicateForNames.cs

---
+++ CSharpAdvanced/04FunctionalProgrammingExercise/07PredicateForNames.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+namespace _06PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> filter = name => name.Length <= n;
+            string[] filtredNames = names.Where(filter).ToArray();            
+            Action<string[]> printer = fn => Console.WriteLine(string.Join(Environment.NewLine, fn)); 
+            printer(filtredNames);            
+        }
+    }
+}
