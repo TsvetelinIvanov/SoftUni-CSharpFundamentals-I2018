0d88f8e752883a1e6b601a964ba00b1bc0b6778e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 09:03:24 +0200

    Create 05HotPotato.cs

---
+++ CSharpAdvanced/01StacksAndQueuesLab/05HotPotato.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace _05HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(children);
+            while(queue.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine("Removed " + queue.Dequeue());
+            }
+
+            Console.WriteLine("Last is " + queue.Dequeue());
+        }
+    }
+}
