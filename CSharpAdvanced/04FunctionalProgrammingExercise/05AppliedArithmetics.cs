b6000bd380037a53a7b3c95eeafcafc350e67ac9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 16:40:08 +0200

    Create 05AppliedArithmetics.cs

---
+++ CSharpAdvanced/04FunctionalProgrammingExercise/05AppliedArithmetics.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            Func<int, int> adder = x => x + 1;
+            Func<int, int> multiplier = x => x * 2;
+            Func<int, int> subtracter = x => x - 1;
+            Action<int> printer = x => Console.Write(x + " ");
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                    numbers = numbers.Select(x => adder(x)).ToList();
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(x => multiplier(x)).ToList();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(x => subtracter(x)).ToList();
+                        break;
+                    case "print":
+                        numbers.ForEach(printer);
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
