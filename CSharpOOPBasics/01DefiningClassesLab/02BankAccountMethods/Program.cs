08068616d7588de77955b473697e7461a09efd6a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-14 20:43:00 +0200

    Add 02BankAccountMethods

---
+++ CSharpOOPBasics/01DefiningClassesLab/02BankAccountMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.Id = 1;
+        bankAccount.Deposit(15);
+        bankAccount.Withdraw(10);
+        Console.WriteLine(bankAccount);
+    }
+}
