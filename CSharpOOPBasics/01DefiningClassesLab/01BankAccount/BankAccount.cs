1345a72571b3d44a5e84914386777ff045ec26e0
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-14 20:22:04 +0200

    Create BankAccount.cs

---
+++ CSharpOOPBasics/01DefiningClassesLab/01BankAccount/BankAccount.cs
@@ -0,0 +1,17 @@
+public class BankAccount
+{
+    private int id;
+    private decimal balance;
+
+    public int Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public decimal Balance
+    {
+        get { return this.balance; }
+        set { this.balance = value; }
+    }
+}
