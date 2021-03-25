0bef9098aa7dec07086ad4245656197f19c4b44d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-25 07:50:39 +0200

    Add 07Workshop

---
+++ CSharpOOPBasics/07Workshop/Forum.App/Controllers/AddReplyController.cs
@@ -0,0 +1,83 @@
+﻿namespace Forum.App.Controllers
+{
+    using Forum.App.Controllers.Contracts;
+    using Forum.App.Services;
+    using Forum.App.UserInterface.Contracts;
+    using Forum.App.UserInterface.Input;
+    using Forum.App.UserInterface.ViewModels;
+    using Forum.App.Views;    
+    using System.Linq;
+
+    public class AddReplyController : IController
+    {
+        private const int TEXT_AREA_WIDTH = 37;
+        private const int TEXT_AREA_HEIGHT = 6;
+        private const int POST_MAX_LENGTH = 220;
+
+        private enum Command
+        {
+            Write, Post
+        }
+
+        private static int centerTop = Position.ConsoleCenter().Top;
+        private static int centerLeft = Position.ConsoleCenter().Left;
+
+        private PostViewModel postViewModel;
+
+        public AddReplyController()
+        {
+            ResetReply();
+        }
+
+        private TextArea TextArea { get; set; }
+
+        public ReplyViewModel Reply { get; private set; }        
+
+        public bool Error { get; private set; }                
+
+        public void ResetReply()
+        {
+            this.Error = false;
+            this.Reply = new ReplyViewModel();
+            int contentLength = postViewModel?.Content.Count ?? 0;
+            this.TextArea = new TextArea(centerLeft - 18, centerTop + contentLength - 6,
+                TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT, POST_MAX_LENGTH);
+        }
+
+        public void SetPostId(int postId)
+        {            
+            postViewModel = PostService.GetPostViewModel(postId);
+            ResetReply();
+        }
+
+        public MenuState ExecuteCommand(int index)
+        {
+            switch ((Command)index)
+            {                
+                case Command.Write:
+                    this.TextArea.Write();
+                    this.Reply.Content = this.TextArea.Lines.ToList();
+                    return MenuState.AddReply;
+                case Command.Post:
+                    bool validReply = PostService.TrySaveReply(this.Reply, postViewModel.PostId);
+                    if (!validReply)
+                    {
+                        this.Error = true;
+
+                        return MenuState.Rerender;
+                    }
+
+                    return MenuState.ReplyAdded;
+            }
+
+            throw new InvalidCommandException();
+        }
+
+        public IView GetView(string userName)
+        {
+            this.Reply.Author = userName;
+
+            return new AddReplyView(this.postViewModel, this.Reply, this.TextArea, this.Error);
+        }
+    }
+}
---
+++ CSharpOOPBasics/07Workshop/Forum.Data/DataMapper.cs
@@ -0,0 +1,188 @@
+﻿namespace Forum.Data
+{
+    using Forum.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class DataMapper
+    {
+        private const string DATA_PATH = "../data/";
+        private const string CONFIG_PATH = "config.ini";
+        private const string DEFAULT_CONFIG = "users=users.csv\r\ncategories=categories.csv\r\nposts=posts.csv" +
+            "\r\nreplies=replies.csv";
+
+        private static readonly Dictionary<string, string> config;
+
+        static DataMapper()
+        {
+            Directory.CreateDirectory(DATA_PATH);
+            config = LoadConfig(DATA_PATH + CONFIG_PATH);
+        }
+
+        private static Dictionary<string, string> LoadConfig(string configPath)
+        {
+            EnsureConfigFile(configPath);
+            string[] contents = ReadLines(configPath);
+            Dictionary<string, string> config = contents.Select(l => l.Split('='))
+                .ToDictionary(t => t[0], t => DATA_PATH + t[1]);
+
+            return config;
+        }
+
+        private static void EnsureConfigFile(string configPath)
+        {
+            if (!File.Exists(configPath))
+            {
+                File.WriteAllText(configPath, DEFAULT_CONFIG);
+            }
+        }
+
+        private static string[] ReadLines(string path)
+        {
+            EnsureFile(path);
+            string[] lines = File.ReadAllLines(path);
+
+            return lines;
+        }
+
+        private static void EnsureFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+        }        
+
+        private static void WriteLines(string path, string[] lines)
+        {
+            File.WriteAllLines(path, lines);
+        }
+
+        public static List<Category> LoadCategories()
+        {
+            List<Category> categories = new List<Category>();
+            string[] dataLines = ReadLines(config["categories"]);
+            foreach (string line in dataLines)
+            {
+                string[] categoryData = line.Split(";", StringSplitOptions.RemoveEmptyEntries);
+                int id = int.Parse(categoryData[0]);
+                string name = categoryData[1];
+                int[] postIds = categoryData[2].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                Category category = new Category(id, name, postIds);
+                categories.Add(category);
+            }
+
+            return categories;
+        }
+
+        public static void SaveCategories(List<Category> categories)
+        {
+            List<string> lines = new List<string>();
+            foreach (Category category in categories)
+            {
+                const string categoryFormat = "{0};{1};{2}";
+                string line = string.Format(categoryFormat, category.Id, category.Name, string.Join(",", category.PostIds));
+                lines.Add(line);
+            }
+
+            WriteLines(config["categories"], lines.ToArray());
+        }
+
+        public static List<User> LoadUsers()
+        {
+            List<User> users = new List<User>();
+            string[] dataLines = ReadLines(config["users"]);
+            foreach (string line in dataLines)
+            {
+                string[] userData = line.Split(";");
+                int id = int.Parse(userData[0]);
+                string username = userData[1];
+                string password = userData[2];
+                int[] postIds = userData[3].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                User user = new User(id, username, password, postIds);
+                users.Add(user);
+            }
+
+            return users;
+        }
+
+        public static void SaveUsers(List<User> users)
+        {
+            List<string> lines = new List<string>();
+            foreach (User user in users)
+            {
+                const string userFormat = "{0};{1};{2};{3}";
+                string line = string.Format(userFormat, user.Id, user.Username, user.Password, string.Join("", user.PostIds));
+                lines.Add(line);
+            }
+
+            WriteLines(config["users"], lines.ToArray());
+        }
+
+        public static List<Post> LoadPosts()
+        {
+            List<Post> posts = new List<Post>();
+            string[] dataLines = ReadLines(config["posts"]);
+            foreach (string line in dataLines)
+            {
+                string[] postData = line.Split(";");
+                int id = int.Parse(postData[0]);
+                string title = postData[1];
+                string content = postData[2];
+                int categoryId = int.Parse(postData[3]);
+                int authorId = int.Parse(postData[4]);
+                int[] replyIds = postData[5].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                Post post = new Post(id,title, content, categoryId, authorId, replyIds);
+                posts.Add(post);
+            }
+
+            return posts;
+        }
+
+        public static void SavePosts(List<Post> posts)
+        {
+            List<string> lines = new List<string>();
+            foreach (Post post in posts)
+            {
+                const string postFormat = "{0};{1};{2};{3};{4};{5}";
+                string line = string.Format(postFormat, post.Id, post.Title, post.Content, post.CategoryId, post.AuthorId, string.Join(",", post.ReplyIds));
+                lines.Add(line);
+            }
+
+            WriteLines(config["posts"], lines.ToArray());
+        }
+
+        public static List<Reply> LoadReplies()
+        {
+            List<Reply> replies = new List<Reply>();
+            string[] dataLines = ReadLines(config["replies"]);
+            foreach (string line in dataLines)
+            {
+                string[] replyData = line.Split(";");
+                int id = int.Parse(replyData[0]);
+                string content = replyData[1];
+                int authorId = int.Parse(replyData[2]);
+                int postId = int.Parse(replyData[3]);
+                Reply reply = new Reply(id, content, authorId, postId);
+                replies.Add(reply);
+            }
+
+            return replies;
+        }
+
+        public static void SaveReplies(List<Reply> replies)
+        {
+            List<string> lines = new List<string>();
+            foreach (Reply reply in replies)
+            {
+                const string replyFormat = "{0};{1};{2};{3}";
+                string line = string.Format(replyFormat, reply.Id, reply.Content, reply.AuthorId, reply.PostId);
+                lines.Add(line);
+            }
+
+            WriteLines(config["replies"], lines.ToArray());
+        }
+    }
+}
