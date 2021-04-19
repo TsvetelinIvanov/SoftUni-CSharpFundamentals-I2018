e751a7ff83ae37527b9a03403bddb75ac9256594
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-19 19:27:53 +0300

    Add 08CreateCustomClassAttribute

---
+++ CSharpOOPAdvanced/04OOPAdvancedReflectionAndAttributesExercises/08CreateCustomClassAttribute/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Type classType = typeof(Weapon);
+        object[] attributes = classType.GetCustomAttributes(false);
+
+        foreach (object attribute in attributes)
+        {
+            ClassAttribute classAttribute = attribute as ClassAttribute;
+            if (classAttribute != null)
+            {
+                string command;
+                while((command = Console.ReadLine()) != "END")
+                {
+                    switch (command)
+                    {
+                        case "Author":
+                            Console.WriteLine($"Author: {classAttribute.Author}");
+                            break;
+                        case "Revision":
+                            Console.WriteLine($"Revision: {classAttribute.Revision}");
+                            break;
+                        case "Description":
+                            Console.WriteLine($"Class description: {classAttribute.Description}");
+                            break;
+                        case "Reviewers":
+                            Console.WriteLine($"Reviewers: {string.Join(", ", classAttribute.Reviewers)}");
+                            break;
+                    }
+                }
+            }
+        }        
+    }
+}
