4b4d200524bfa064062b21da71c96bb5ff3af826
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-19 19:15:22 +0300

    Add 02OOPAdvancedGenericsLab

---
+++ CSharpOOPAdvanced/02OOPAdvancedGenericsLab/01BoxOfT/Box.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Box<T>
+{
+    private List<T> data;
+
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public int Count => this.data.Count;
+
+    public void Add(T item)
+    {
+        this.data.Add(item);
+    }
+
+    public T Remove()
+    {
+        T removedItem = this.data.Last();
+        this.data.RemoveAt(this.data.Count - 1);
+
+        return removedItem;
+    }
+}
---
+++ CSharpOOPAdvanced/02OOPAdvancedGenericsLab/02GenericArrayCreator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strings = ArrayCreator.Create(5, "Pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+
+        foreach (string item in strings)
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (int item in integers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
