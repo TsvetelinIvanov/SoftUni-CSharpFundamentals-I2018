5b560b79a6f294a324aa514964f95de08280598d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-29 21:36:20 +0300

    Update 05CalculateSequenceWithQueue.cs

--- CSharpAdvanced/01StacksAndQueuesExercise/05CalculateSequenceWithQueue.cs
+++ CSharpAdvanced/01StacksAndQueuesExercise/05CalculateSequenceWithQueue.cs
@@ -8,20 +8,25 @@ namespace _05CalculateSequenceWithQueue
         static void Main(string[] args)
         {
             long n = long.Parse(Console.ReadLine());
+            
             Queue<long> queue = new Queue<long>();
-            Queue<long> queueCurrentS = new Queue<long>();
-            queueCurrentS.Enqueue(n);
             queue.Enqueue(n);
+            
+            Queue<long> queueCurrentS = new Queue<long>();
+            queueCurrentS.Enqueue(n);            
 
             for (int i = 0; i < 17; i++)
             {
                 long s1 = queueCurrentS.Dequeue();
+                
                 long s2 = s1 + 1;
                 queue.Enqueue(s2);
                 queueCurrentS.Enqueue(s2);
+                
                 long s3 = 2 * s1 + 1;
                 queue.Enqueue(s3);
                 queueCurrentS.Enqueue(s3);
+                
                 long s4 = s1 + 2;
                 queue.Enqueue(s4);
                 queueCurrentS.Enqueue(s4);
