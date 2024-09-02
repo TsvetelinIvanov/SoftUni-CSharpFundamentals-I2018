3b4188f5d5fa1652efbb13af76e8b6305c27ad9f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-02 20:40:22 +0300

    Update 06ZippingSlicedFiles.cs

--- CSharpAdvanced/03StreamsAndFilesExercise/06ZippingSlicedFiles.cs
+++ CSharpAdvanced/03StreamsAndFilesExercise/06ZippingSlicedFiles.cs
@@ -7,16 +7,16 @@ namespace _06ZippingSlicedFiles
 {
     class Program
     {
-        private const int bufferSize = 4096;
+        private const int BufferSize = 4096;
 
         static void Main(string[] args)
         {
             string sourceFile = "../Resources/sliceMe.mp4";            
             string destination = "";
-            int parts = 5;
-            Slice(sourceFile, destination, parts);
+            int partsCount = 5;
+            Slice(sourceFile, destination, partsCount);
 
-            Zip(sourceFile, destination, parts);
+            Zip(sourceFile, destination, partsCount);
 
             List<string> files = new List<string>
             {
@@ -30,13 +30,13 @@ namespace _06ZippingSlicedFiles
             Assemble(files, destination);
         }
 
-        static void Slice(string sourceFile, string destinationDirectory, int parts)
+        static void Slice(string sourceFile, string destinationDirectory, int partsCount)
         {
             using (FileStream reader = new FileStream(sourceFile, FileMode.Open))
             {
                 string extention = sourceFile.Substring(sourceFile.LastIndexOf('.') + 1);
-                long pieceSize = (long)Math.Ceiling((double)reader.Length / parts);
-                for (int i = 0; i < parts; i++)
+                long pieceSize = (long)Math.Ceiling((double)reader.Length / partsCount);
+                for (int i = 0; i < partsCount; i++)
                 {
                     long currentPieceSize = 0;
                     if (destinationDirectory == String.Empty)
@@ -48,11 +48,11 @@ namespace _06ZippingSlicedFiles
 
                     using (FileStream writer = new FileStream(currentPart, FileMode.Create))
                     {
-                        byte[] buffer = new byte[bufferSize];
-                        while (reader.Read(buffer, 0, bufferSize) == bufferSize)
+                        byte[] buffer = new byte[BufferSize];
+                        while (reader.Read(buffer, 0, BufferSize) == BufferSize)
                         {
-                            writer.Write(buffer, 0, bufferSize);
-                            currentPieceSize += bufferSize;
+                            writer.Write(buffer, 0, BufferSize);
+                            currentPieceSize += BufferSize;
                             if (currentPieceSize >= pieceSize)
                             {
                                 break;
@@ -63,15 +63,15 @@ namespace _06ZippingSlicedFiles
             }
         }
 
-        static void Zip(string sourceFile, string destinationDirectory, int parts)
+        static void Zip(string sourceFile, string destinationDirectory, int partsCount)
         {
 
             using (GZipStream reader = new GZipStream(new FileStream(sourceFile, FileMode.Open), CompressionLevel.Optimal))          
             {
                 string extention = sourceFile.Substring(sourceFile.LastIndexOf('.') + 1);
-                long pieceSize = (long)Math.Ceiling((double)reader.Length / parts);
+                long pieceSize = (long)Math.Ceiling((double)reader.Length / partsCount);
 
-                for (int i = 0; i < parts; i++)
+                for (int i = 0; i < partsCount; i++)
                 {
                     long currentPieceSize = 0;
                     if (destinationDirectory == String.Empty)
@@ -81,14 +81,13 @@ namespace _06ZippingSlicedFiles
 
                     string currentPart = destinationDirectory + $"Part-{i}.{extention}.gz";
 
-                    using (GZipStream writer = new GZipStream(new FileStream(currentPart, FileMode.Create),
-                        CompressionLevel.Optimal))
+                    using (GZipStream writer = new GZipStream(new FileStream(currentPart, FileMode.Create), CompressionLevel.Optimal))
                     {
-                        byte[] buffer = new byte[bufferSize];
-                        while (reader.Read(buffer, 0, bufferSize) == bufferSize)
+                        byte[] buffer = new byte[BufferSize];
+                        while (reader.Read(buffer, 0, BufferSize) == BufferSize)
                         {
-                            writer.Write(buffer, 0, bufferSize);
-                            currentPieceSize += bufferSize;
+                            writer.Write(buffer, 0, BufferSize);
+                            currentPieceSize += BufferSize;
                             if (currentPieceSize >= pieceSize)
                             {
                                 break;
@@ -117,14 +116,14 @@ namespace _06ZippingSlicedFiles
 
             using (FileStream writer = new FileStream(assembledFile, FileMode.Create))
             {
-                byte[] buffer = new byte[bufferSize];
+                byte[] buffer = new byte[BufferSize];
                 foreach (string file in files)
                 {
                     using (FileStream reader = new FileStream(file, FileMode.Open))
                     {
-                        while (reader.Read(buffer, 0, bufferSize) == bufferSize)
+                        while (reader.Read(buffer, 0, BufferSize) == BufferSize)
                         {
-                            writer.Write(buffer, 0, bufferSize);
+                            writer.Write(buffer, 0, BufferSize);
                         }
                     }
                 }
