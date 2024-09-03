9f29fb6d1f4f1b854e98055a6b786c21dfe949f7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-03 20:14:22 +0300

    Update 13ThreeFunction.cs

--- CSharpAdvanced/04FunctionalProgrammingExercise/13ThreeFunction.cs
+++ CSharpAdvanced/04FunctionalProgrammingExercise/13ThreeFunction.cs
@@ -10,8 +10,8 @@ namespace _13ThreeFunction
             int number = int.Parse(Console.ReadLine());
             string[] names = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             Func<string, int, bool> filter = (name, num) => name.ToCharArray().Sum(ch => ch) >= num;
-            string firstLargerName = names.FirstOrDefault(name => filter(name, number));
-            Console.WriteLine(firstLargerName);
+            string firstLargeName = names.FirstOrDefault(name => filter(name, number));
+            Console.WriteLine(firstLargeName);
         }
     }
 }
