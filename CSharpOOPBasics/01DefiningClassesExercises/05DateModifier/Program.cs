93c2f039dc447c63c332459051105db8e862014f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-12 20:45:49 +0300

    Update Program.cs

--- CSharpOOPBasics/01DefiningClassesExercises/05DateModifier/Program.cs
+++ CSharpOOPBasics/01DefiningClassesExercises/05DateModifier/Program.cs
@@ -7,9 +7,11 @@ public class Program
     {
         string firstDateString = Console.ReadLine();
         string secondDateString = Console.ReadLine();
+        
         DateModifier dateModifier = new DateModifier();
         dateModifier.FirstDate = DateTime.ParseExact(firstDateString, "yyyy MM dd", CultureInfo.InvariantCulture);
         dateModifier.SecondDate = DateTime.ParseExact(secondDateString, "yyyy MM dd", CultureInfo.InvariantCulture);
+        
         long diferenceInDays = dateModifier.GiveDiferenceInDays();
         Console.WriteLine(diferenceInDays);
     }
