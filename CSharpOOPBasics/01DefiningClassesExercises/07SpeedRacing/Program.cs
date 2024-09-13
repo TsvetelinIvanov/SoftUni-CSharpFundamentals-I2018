82c00701c8cf762f8b83da5d2a92246081b994b7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-13 13:50:50 +0300

    Update Program.cs

--- CSharpOOPBasics/01DefiningClassesExercises/07SpeedRacing/Program.cs
+++ CSharpOOPBasics/01DefiningClassesExercises/07SpeedRacing/Program.cs
@@ -10,20 +10,28 @@
 
         public static void Main()
         {
-            var cars = GetCars();
+            Queue<Car> cars = GetCars();
             DriveCars(cars);
             PrintCars(cars);
         }
 
-        private static void PrintCars(Queue<Car> cars)
+        private static Queue<Car> GetCars()
         {
-            Console.WriteLine(string.Join(Environment.NewLine, cars
-                .Select(c => $"{c.Model} {c.FuelAmount:F2} {c.DistanceTravelled}")));
+            Queue<Car> cars = new Queue<Car>();
+            int carsNumber = int.Parse(Console.ReadLine());
+
+            while (cars.Count < carsNumber)
+            {
+                string[] inputCarData = Console.ReadLine().Split();
+                cars.Enqueue(new Car(inputCarData[0], double.Parse(inputCarData[1]), double.Parse(inputCarData[2])));
+            }
+
+            return cars;
         }
 
         private static void DriveCars(Queue<Car> cars)
         {
-            var command = Console.ReadLine().Split();
+            string[] command = Console.ReadLine().Split();
 
             while (command[0] != "End")
             {
@@ -37,18 +45,9 @@
             }
         }
 
-        private static Queue<Car> GetCars()
+        private static void PrintCars(Queue<Car> cars)
         {
-            Queue<Car> cars = new Queue<Car>();
-            int carsNumber = int.Parse(Console.ReadLine());
-
-            while (cars.Count < carsNumber)
-            {
-                string[] input = Console.ReadLine().Split();
-                cars.Enqueue(new Car(input[0], double.Parse(input[1]), double.Parse(input[2])));
-            }
-
-            return cars;
+            Console.WriteLine(string.Join(Environment.NewLine, cars.Select(c => $"{c.Model} {c.FuelAmount:F2} {c.DistanceTravelled}")));
         }
     }
 }
