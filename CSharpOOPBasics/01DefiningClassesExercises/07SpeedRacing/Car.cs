9ac79b14311b3a7467e63dedc645e01f6656ac6a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-23 15:13:17 +0300

    Update Car.cs

--- CSharpOOPBasics/01DefiningClassesExercises/07SpeedRacing/Car.cs
+++ CSharpOOPBasics/01DefiningClassesExercises/07SpeedRacing/Car.cs
@@ -5,14 +5,14 @@
         private string model;
         private double fuelAmount;
         private double fuelConsumptionPerKm;
-        private double distanceTravelled;
+        private double traveledDistance;
 
         public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
         {
-            this.model = model;
-            this.fuelAmount = fuelAmount;
+            this.model = model;            
             this.fuelConsumptionPerKm = fuelConsumptionPerKm;
-            this.distanceTravelled = 0.0;
+            this.FuelAmount = fuelAmount;
+            this.TraveledDistance = 0.0;
         }
 
         public string Model
@@ -31,25 +31,25 @@
             get { return this.fuelConsumptionPerKm; }
         }
 
-        public double DistanceTravelled
+        public double TravelledDistance
         {
-            get { return this.distanceTravelled; }
-            set { this.distanceTravelled = value; }
+            get { return this.traveledDistance; }
+            set { this.traveledDistance = value; }
         }
 
         public void Drive(double kilometers)
         {
-            double neededFuel = kilometers * this.fuelConsumptionPerKm;
+            double neededFuel = kilometers * this.FuelConsumptionPerKm;
 
-            if (this.fuelAmount < neededFuel)
+            if (this.FuelAmount < neededFuel)
             {
                 System.Console.WriteLine("Insufficient fuel for the drive");
                 
                 return;
             }
 
-            this.fuelAmount -= neededFuel;
-            this.distanceTravelled += kilometers;
+            this.FuelAmount -= neededFuel;
+            this.TraveledDistance += kilometers;
         }
     }
 }
