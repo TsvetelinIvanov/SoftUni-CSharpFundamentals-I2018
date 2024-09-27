ad2d08114d5e649df8f02be8ff143818f739d3f5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-27 14:50:54 +0300

    Update Program.cs

--- CSharpOOPBasics/02WokingWithAbstractionExercises/07FamilyTree/Program.cs
+++ CSharpOOPBasics/02WokingWithAbstractionExercises/07FamilyTree/Program.cs
@@ -20,38 +20,25 @@ public class Program
         {
             if (inputLine.Contains("-"))
             {
-                string[] tokens = inputLine
-                    .Split('-')
-                    .Select(x => x.Trim())
-                    .ToArray();
-
+                string[] tokens = inputLine.Split('-').Select(x => x.Trim()).ToArray();
                 string parentParam = tokens[0];
                 string childParam = tokens[1];
                 
-                Person parent = allPeople.FirstOrDefault(p => (parentParam.Contains("/"))
-                    ? p.BirthDate == parentParam
-                    : p.Name == parentParam);
-
+                Person parent = allPeople.FirstOrDefault(p => (parentParam.Contains("/")) ? p.BirthDate == parentParam : p.Name == parentParam);
                 if (parent == null)
                 {
-                    parent = (parentParam.Contains("/"))
-                        ? new Person { BirthDate = parentParam }
-                        : new Person { Name = parentParam };
-
+                    parent = (parentParam.Contains("/")) ? new Person { BirthDate = parentParam } : new Person { Name = parentParam };
                     allPeople.Add(parent);
                 }
                                 
-                Person child = (childParam.Contains("/"))
-                    ? new Person { BirthDate = childParam }
-                    : new Person { Name = childParam };
-
+                Person child = (childParam.Contains("/")) ? new Person { BirthDate = childParam } : new Person { Name = childParam };
                 parent.AddChild(child);
             }
             else
             {
-                string[] tokens = inputLine.Split();
-                string name = $"{tokens[0]} {tokens[1]}";
-                string date = tokens[2];
+                string[] personData = inputLine.Split();
+                string name = $"{personData[0]} {personData[1]}";
+                string date = personData[2];
                 bool added = false;
 
                 for (int i = 0; i < allPeople.Count; i++)
@@ -81,25 +68,23 @@ public class Program
 
     private static void PrintParentsAndChildren(List<Person> allPeople, string searchedPersonParam)
     {
-        Person person = allPeople.FirstOrDefault(p => (searchedPersonParam.Contains("/"))
-            ? p.BirthDate == searchedPersonParam
-            : p.Name == searchedPersonParam);
+        Person person = allPeople.FirstOrDefault(p => (searchedPersonParam.Contains("/")) ? p.BirthDate == searchedPersonParam : p.Name == searchedPersonParam);
 
-        StringBuilder result = new StringBuilder();
-        result.AppendLine($"{person.Name} {person.BirthDate}");
+        StringBuilder familyTreeBuilder = new StringBuilder();
+        familyTreeBuilder.AppendLine($"{person.Name} {person.BirthDate}");
 
-        result.AppendLine("Parents:");
+        familyTreeBuilder.AppendLine("Parents:");
         foreach (Person parent in allPeople.Where(p => p.FindChildName(person.Name) != null))
         {
-            result.AppendLine($"{parent.Name} {parent.BirthDate}");
+            familyTreeBuilder.AppendLine($"{parent.Name} {parent.BirthDate}");
         }
 
-        result.AppendLine("Children:");
+        familyTreeBuilder.AppendLine("Children:");
         foreach (Person child in allPeople.FirstOrDefault(p => p.Name == person.Name).Children)
         {
-            result.AppendLine($"{child.Name} {child.BirthDate}");
+            familyTreeBuilder.AppendLine($"{child.Name} {child.BirthDate}");
         }
 
-        Console.WriteLine(result);
+        Console.WriteLine(familyTreeBuilder);
     }    
 }
