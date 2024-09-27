2856f78ffc0d53f70fa3e6e3b21c68750bf16623
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-27 15:29:18 +0300

    Update Point.cs

--- CSharpOOPBasics/02WorkingWithAbstractionLab/02PointInRectangle/Point.cs
+++ CSharpOOPBasics/02WorkingWithAbstractionLab/02PointInRectangle/Point.cs
@@ -2,10 +2,11 @@
 {
     public Point(int x, int y)
     {
-        X = x;
-        Y = y;
+        this.X = x;
+        this.Y = y;
     }
 
     public int X { get; set; }
+    
     public int Y { get; set; }
 }
