a96f4d496564e948a1dafe2b9d358cf74cf3262e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-27 15:36:10 +0300

    Update Rectangle.cs

--- CSharpOOPBasics/02WorkingWithAbstractionLab/02PointInRectangle/Rectangle.cs
+++ CSharpOOPBasics/02WorkingWithAbstractionLab/02PointInRectangle/Rectangle.cs
@@ -2,17 +2,18 @@
 {
     public Rectangle(int topX, int topY, int bottomX, int bottomY)
     {
-        TopLeft = new Point(topX, topY);
-        BottomRight = new Point(bottomX, bottomY);
+        this.TopLeft = new Point(topX, topY);
+        this.BottomRight = new Point(bottomX, bottomY);
     }
 
     public Point TopLeft { get; set; }
+    
     public Point BottomRight { get; set; }    
 
     public bool Contains(Point point)
     {
-        bool isInHorizontal = TopLeft.X <= point.X && BottomRight.X >= point.X;
-        bool isInVertical = TopLeft.Y <= point.Y && BottomRight.Y >= point.Y;
+        bool isInHorizontal = this.TopLeft.X <= point.X && this.BottomRight.X >= point.X;
+        bool isInVertical = this.TopLeft.Y <= point.Y && this.BottomRight.Y >= point.Y;
         bool isInRectangle = isInHorizontal && isInVertical;
 
         return isInRectangle;
