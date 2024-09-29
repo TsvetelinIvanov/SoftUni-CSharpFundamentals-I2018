92ca185e7dae5e1f3cd7a9313e07f3947d355b66
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-29 15:48:52 +0300

    Update Frog.cs

--- CSharpOOPBasics/04InheritanceExercises/06Animals/Animals/Frog.cs
+++ CSharpOOPBasics/04InheritanceExercises/06Animals/Animals/Frog.cs
@@ -2,8 +2,7 @@
 {
     private const string ProducedSound = "Ribbit";
 
-    public Frog(string name, int age, string gender)
-        : base(name, age, gender)
+    public Frog(string name, int age, string gender) : base(name, age, gender)
     {
 
     }
