7a2052d501661268967d790f0d6c258161fa8aba
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-29 15:48:24 +0300

    Update Dog.cs

--- CSharpOOPBasics/04InheritanceExercises/06Animals/Animals/Dog.cs
+++ CSharpOOPBasics/04InheritanceExercises/06Animals/Animals/Dog.cs
@@ -2,8 +2,7 @@
 {
     private const string ProducedSound = "Woof!";
 
-    public Dog(string name, int age, string gender)
-        : base(name, age, gender)
+    public Dog(string name, int age, string gender) : base(name, age, gender)
     {
 
     }
