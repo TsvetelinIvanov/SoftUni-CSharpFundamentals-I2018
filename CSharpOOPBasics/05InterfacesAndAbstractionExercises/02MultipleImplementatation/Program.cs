c4fdf289af0f69f7c93a80c0eba299745ce76fa9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-30 12:45:21 +0300

    Update Program.cs

--- CSharpOOPBasics/05InterfacesAndAbstractionExercises/02MultipleImplementatation/Program.cs
+++ CSharpOOPBasics/05InterfacesAndAbstractionExercises/02MultipleImplementatation/Program.cs
@@ -8,8 +8,10 @@ public class Program
         int age = int.Parse(Console.ReadLine());
         string id = Console.ReadLine();
         string birthdate = Console.ReadLine();
+        
         IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
         IBirthable ibirthable = new Citizen(name, age, id, birthdate);
+        
         Console.WriteLine(identifiable.Id);
         Console.WriteLine(ibirthable.Birthdate);
     }
