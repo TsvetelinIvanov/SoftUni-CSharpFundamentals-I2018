1de56758649f4909c968ce3068340dfd7f37b9a4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-30 12:40:07 +0300

    Update Program.cs

--- CSharpOOPBasics/05InterfacesAndAbstractionExercises/01DefineAnInterfaceIPerson/Program.cs
+++ CSharpOOPBasics/05InterfacesAndAbstractionExercises/01DefineAnInterfaceIPerson/Program.cs
@@ -7,6 +7,7 @@ public class Program
         string name = Console.ReadLine();
         int age = int.Parse(Console.ReadLine());
         IPerson person = new Citizen(name, age);
+        
         Console.WriteLine(person.Name);
         Console.WriteLine(person.Age);
     }
