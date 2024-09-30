077132be0f4c107bfce3a8373c7d90030b65f08e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-09-30 15:36:03 +0300

    Update Tesla.cs

--- CSharpOOPBasics/05InterfacesAndAbstractionLab/02Cars/Tesla.cs
+++ CSharpOOPBasics/05InterfacesAndAbstractionLab/02Cars/Tesla.cs
@@ -1,7 +1,6 @@
 ﻿public class Tesla : ElectricCar
 {
-    public Tesla(string model, string color, int battery)
-        : base(model, color, battery)
+    public Tesla(string model, string color, int battery) : base(model, color, battery)
     {
 
     }
