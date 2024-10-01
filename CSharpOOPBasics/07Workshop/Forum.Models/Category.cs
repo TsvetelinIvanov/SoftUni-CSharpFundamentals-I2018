9e6192f161ab2cefbe22ff3d6dc4aedbe9276f0f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-01 17:31:31 +0300

    Add Category.cs

---
+++ CSharpOOPBasics/07Workshop/Forum.Models/Category.cs
@@ -0,0 +1,36 @@
+﻿namespace Forum.Models
+{
+    using System.Collections.Generic;
+
+    public class Category
+    {
+        private int id;
+        private string name;
+        private ICollection<int> postIds;
+
+        public Category(int id, string name, IEnumerable<int> postIds)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.PostIds = new List<int>(postIds);
+        }
+
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public ICollection<int> PostIds
+        {
+            get { return this.postIds; }
+            set { this.postIds = value; }
+        }
+    }
+}
