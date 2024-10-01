4581201ffe641ab489f9e23aa307c20d65bbab06
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-01 15:29:27 +0300

    Update Tiger.cs

--- CSharpOOPBasics/06PolmorphismExercises/03WildFarm/Animals/Tiger.cs
+++ CSharpOOPBasics/06PolmorphismExercises/03WildFarm/Animals/Tiger.cs
@@ -4,8 +4,7 @@ public class Tiger : Feline
 {
     private const double WeightFoodMultiplier = 1;
 
-    public Tiger(string name, double weight, string livingRegion, string breed)
-        : base(name, weight, livingRegion, breed)
+    public Tiger(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, breed)
     {
 
     }
