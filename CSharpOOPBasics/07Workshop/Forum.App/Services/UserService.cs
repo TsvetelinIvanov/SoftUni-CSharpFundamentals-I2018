b2f55525227bfdcdd3bf04305cb45250ff257fc9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-03 12:37:30 +0300

    Update UserService.cs

--- CSharpOOPBasics/07Workshop/Forum.App/Services/UserService.cs
+++ CSharpOOPBasics/07Workshop/Forum.App/Services/UserService.cs
@@ -1,15 +1,39 @@
 ﻿namespace Forum.App.Services
 {
-    using Forum.Data;
-    using Forum.Models;
     using System.Collections.Generic;
     using System.Linq;
+    using Forum.Data;
+    using Forum.Models;    
     using static Forum.App.Controllers.SignUpController;
 
     public static class UserService
     {
         private const int USERNAME_MIN_LENGTH = 4;
         private const int PASSWORD_MIN_LENGTH = 4;
+
+        public static SignUpStatus TrySignUpUser(string username, string password)
+        {
+            bool validUsername = !string.IsNullOrWhiteSpace(username) && username.Length >= USERNAME_MIN_LENGTH;
+            bool validPassword = !string.IsNullOrWhiteSpace(password) && password.Length >= PASSWORD_MIN_LENGTH;
+            if (!validPassword || !validUsername)
+            {
+                return SignUpStatus.DetailsError;
+            }
+
+            ForumData forumData = new ForumData();
+            bool userAlreaedyExists = forumData.Users.Any(u => u.Username == username);
+            if (userAlreaedyExists)
+            {
+                return SignUpStatus.UsernameTakenError;                
+            }
+
+            int userId = forumData.Users.LastOrDefault()?.Id + 1 ?? 1;
+            User user = new User(userId, username, password, new List<int>());
+            forumData.Users.Add(user);
+            forumData.SaveChanges();
+
+            return SignUpStatus.Success;
+        }
 
         public static bool TryLogInUser(string username, string password)
         {
@@ -24,36 +48,11 @@
             return userExist;
         }
 
-        public static SignUpStatus TrySignUpUser(string username, string password)
-        {
-            bool validUsername = !string.IsNullOrWhiteSpace(username) && username.Length >= USERNAME_MIN_LENGTH;
-            bool validPassword = !string.IsNullOrWhiteSpace(password) && password.Length >= PASSWORD_MIN_LENGTH;
-
-            if (!validPassword || !validUsername)
-            {
-                return SignUpStatus.DetailsError;
-            }
-
-            ForumData forumData = new ForumData();
-            bool userAlreaedyExists = forumData.Users.Any(u => u.Username == username);
-            if (!userAlreaedyExists)
-            {
-                int userId = forumData.Users.LastOrDefault()?.Id + 1 ?? 1;
-                User user = new User(userId, username, password, new List<int>());
-                forumData.Users.Add(user);
-                forumData.SaveChanges();
-
-                return SignUpStatus.Success;
-            }
-
-            return SignUpStatus.UsernameTakenError;
-        }
-
         internal static User GetUser(int userId)
         {
             ForumData forumData = new ForumData();
-            //User user = forumData.Users.Find(u => u.Id == userId);
-            User user = forumData.Users.Single(u => u.Id == userId);
+            User user = forumData.Users.Find(u => u.Id == userId);
+            //User user = forumData.Users.Single(u => u.Id == userId);
 
             return user;
         }
