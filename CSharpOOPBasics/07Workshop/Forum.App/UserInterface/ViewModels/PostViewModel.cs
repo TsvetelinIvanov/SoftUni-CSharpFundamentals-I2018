79a351f8aa2c2da3c0253e0cc260ee7e2ba1e3c9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-03 17:20:22 +0300

    Update PostViewModel.cs

--- CSharpOOPBasics/07Workshop/Forum.App/UserInterface/ViewModels/PostViewModel.cs
+++ CSharpOOPBasics/07Workshop/Forum.App/UserInterface/ViewModels/PostViewModel.cs
@@ -1,9 +1,9 @@
 ﻿namespace Forum.App.UserInterface.ViewModels
 {
-    using Forum.App.Services;
-    using Forum.Models;    
     using System.Collections.Generic;
     using System.Linq;
+    using Forum.App.Services;
+    using Forum.Models;   
 
     public class PostViewModel
     {
