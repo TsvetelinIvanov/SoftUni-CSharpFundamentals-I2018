24cd980148dac398c8f3948668782cb36298a96c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-08 11:58:41 +0300

    Update InvalidFileNameException.cs

--- CSharpOOPBasics/BashSoftOOPBasics/BashSoft/Exceptions/InvalidFileNameException.cs
+++ CSharpOOPBasics/BashSoftOOPBasics/BashSoft/Exceptions/InvalidFileNameException.cs
@@ -4,9 +4,9 @@ namespace BashSoft.Exceptions
 {
     public class InvalidFileNameException : Exception
     {
-        private const string ForbiddenSymbolsContainedInName = "The given name contains symbols that are not allowed to be used in names of files and folders!";
+        private const string ForbiddenSymbolsContainedInNameExcepionMessage = "The given name contains symbols that are not allowed to be used in names of files and folders!";
 
-        public InvalidFileNameException() : base(ForbiddenSymbolsContainedInName)
+        public InvalidFileNameException() : base(ForbiddenSymbolsContainedInNameExcepionMessage)
         {
 
         }
