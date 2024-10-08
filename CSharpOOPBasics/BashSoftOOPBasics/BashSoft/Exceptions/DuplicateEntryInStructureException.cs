a6bffde3f088b10c7460209df287361112cdef8c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-08 11:55:38 +0300

    Update DuplicateEntryInStructureException.cs

--- CSharpOOPBasics/BashSoftOOPBasics/BashSoft/Exceptions/DuplicateEntryInStructureException.cs
+++ CSharpOOPBasics/BashSoftOOPBasics/BashSoft/Exceptions/DuplicateEntryInStructureException.cs
@@ -4,9 +4,9 @@ namespace BashSoft.Exceptions
 {
     public class DuplicateEntryInStructureException : Exception
     {
-        private const string DuplicateEntry = "The {0} already exists in {1}!";        
+        private const string DuplicateEntryExcepionMessage = "The {0} already exists in {1}!";        
 
-        public DuplicateEntryInStructureException(string entry, string structure) : base(string.Format(DuplicateEntry, entry, structure))
+        public DuplicateEntryInStructureException(string entry, string structure) : base(string.Format(DuplicateEntryExcepionMessage, entry, structure))
         {
 
         }
