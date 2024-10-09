1b9b76c80b6b20d0527eb92949375b5ba4956dda
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-09 18:28:04 +0300

    Update Program.cs

--- CSharpOOPBasics/BashSoftOOPBasics/BashSoft/Program.cs
+++ CSharpOOPBasics/BashSoftOOPBasics/BashSoft/Program.cs
@@ -12,6 +12,7 @@ namespace BashSoft
             Tester tester = new Tester();
             IOManager ioManager = new IOManager();
             StudentsRepository repo = new StudentsRepository(new RepositorySorter(), new RepositoryFilter());
+            
             CommandInterpreter currentInterpreter = new CommandInterpreter(tester, repo, ioManager);
             InputReader reader = new InputReader(currentInterpreter);
 
