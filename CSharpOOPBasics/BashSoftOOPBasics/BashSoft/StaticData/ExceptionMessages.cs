ab71ba95d0a16ee78521b219fe7282fc4a5ce10a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-09 17:13:16 +0300

    Update ExceptionMessages.cs

--- CSharpOOPBasics/BashSoftOOPBasics/BashSoft/StaticData/ExceptionMessages.cs
+++ CSharpOOPBasics/BashSoftOOPBasics/BashSoft/StaticData/ExceptionMessages.cs
@@ -8,9 +8,9 @@
 
         public const string DataNotInitializedExceptionMessage = "The data structure must be initialized first in order to make any operation with it!";
 
-        public const string InexistingCourseInDataBase = "The course you are trying to get does not exist in the data base!";
+        public const string InexistantCourseInDataBase = "The course you are trying to get does not exist in the data base!";
 
-        public const string InexistingStudentInDataBase = "The user name for the student you are trying to get does not exist!";
+        public const string InexistantStudentInDataBase = "The user name for the student you are trying to get does not exist!";
 
         public const string InvalidPath = "The folder/file you are trying to access at the current address, does not exist!";
 
