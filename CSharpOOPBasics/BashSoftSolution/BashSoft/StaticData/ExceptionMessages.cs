fb98f05dfdfb9128e9d226f4b0c20c6800f2e7a0
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-11 14:51:03 +0300

    Update ExceptionMessages.cs

--- CSharpOOPBasics/BashSoftSolution/BashSoft/StaticData/ExceptionMessages.cs
+++ CSharpOOPBasics/BashSoftSolution/BashSoft/StaticData/ExceptionMessages.cs
@@ -8,7 +8,7 @@
 
         public const string DataNotInitializedExceptionMessage = "The data structure must be initialized first in order to make any operation with it.";
 
-        public const string InexistantCourseInDataBase = "The course you are trying to get does not exist in the data base!";
+        public const string InexistantCourseInDatabase = "The course you are trying to get does not exist in the data base!";
 
         public const string InexistantStudentInDatabase = "The user name for the student you are trying to get does not exist!";
 
@@ -22,7 +22,7 @@
 
         public const string UnableToGoHigherInPartitionHierarchy = "The program is unable to go higher in partition hierarchy!";
 
-        public const string UnableToParseNumberExceptionMessage = "The sequence ypu've written is not a valid number.";
+        public const string UnableToParseNumberExceptionMessage = "The sequence you have written is not a valid number.";
 
         public const string InvalidStudentFilterExceptionMessage = "The given filter is not one of the following: excellent/average/poor!";
 
