67f7245469b84da60046ac884d9cdd2711aeb69a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-14 16:33:53 +0300

    Update HammerHarvester.cs

--- CSharpOOPBasics/ExamOOPBasics16VII2017/Minedraft/Units/Harvesters/HammerHarvester.cs
+++ CSharpOOPBasics/ExamOOPBasics16VII2017/Minedraft/Units/Harvesters/HammerHarvester.cs
@@ -3,7 +3,8 @@
     private const double OreOutputIncreaser = 3;
     private const double EnergyRequirementIncreaser = 2;
 
-    public HammerHarvester(string id, double oreOutput, double energyRequirement) : base(id, oreOutput * OreOutputIncreaser, energyRequirement * EnergyRequirementIncreaser)
+    public HammerHarvester(string id, double oreOutput, double energyRequirement)
+        : base(id, oreOutput * OreOutputIncreaser, energyRequirement * EnergyRequirementIncreaser)
     {
 
     }
