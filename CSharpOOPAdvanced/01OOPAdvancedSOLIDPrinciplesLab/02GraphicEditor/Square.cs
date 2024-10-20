3128fead5802fd718af2b82f4a0c4503ac0990cf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-10-20 15:46:11 +0300

    Update Square.cs

--- CSharpOOPAdvanced/01OOPAdvancedSOLIDPrinciplesLab/02GraphicEditor/Square.cs
+++ CSharpOOPAdvanced/01OOPAdvancedSOLIDPrinciplesLab/02GraphicEditor/Square.cs
@@ -6,7 +6,7 @@ namespace _02.Graphic_Editor
     {
         public void Draw()
         {
-            Console.WriteLine("I'm Square");
+            Console.WriteLine("I'm a Square");
         }
     }
 }
