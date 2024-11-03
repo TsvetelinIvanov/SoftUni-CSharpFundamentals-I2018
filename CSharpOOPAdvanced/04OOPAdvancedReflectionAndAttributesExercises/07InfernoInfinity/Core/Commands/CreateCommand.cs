fd26f397eddaaa40e1510d71858b51852f2e485a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-03 12:02:53 +0200

    Update CreateCommand.cs

--- CSharpOOPAdvanced/04OOPAdvancedReflectionAndAttributesExercises/07InfernoInfinity/Core/Commands/CreateCommand.cs
+++ CSharpOOPAdvanced/04OOPAdvancedReflectionAndAttributesExercises/07InfernoInfinity/Core/Commands/CreateCommand.cs
@@ -13,9 +13,10 @@
         string[] weaponData = this.Data[0].Split();
         string rarity = weaponData[0];
         string type = weaponData[1];
+        
         string name = this.Data[1];
-
         IWeapon weapon = this.weaponFactory.CreateWeapon(rarity, type, name);
+        
         this.repository.AddWeapon(weapon);
     }
 }
