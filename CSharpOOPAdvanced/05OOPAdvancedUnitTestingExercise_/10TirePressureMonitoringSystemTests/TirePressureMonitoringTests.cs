440e0b26dd8f9025bf73406b255e5071cd80d079
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-09 16:47:14 +0200

    Update TirePressureMonitoringTests.cs

--- CSharpOOPAdvanced/05OOPAdvancedUnitTestingExercise_/10TirePressureMonitoringSystemTests/TirePressureMonitoringTests.cs
+++ CSharpOOPAdvanced/05OOPAdvancedUnitTestingExercise_/10TirePressureMonitoringSystemTests/TirePressureMonitoringTests.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Reflection;
-using _10TirePressureMonitoringSystem;
 using Moq;
 using NUnit.Framework;
+using _10TirePressureMonitoringSystem;
 
 namespace _10TirePressureMonitoringSystemTests
 {
     [TestFixture]
     public class TirePressureMonitoringTests
     {
+        private const int ValidPrssure = 19;
+        private const int BottomValidPressure = 17;
+        private const int TopValidPressure = 21;
         private const int NegativePressure = -10;
         private const int LowerPressure = 15;
-        private const int BottomValidPressure = 17;
-        private const int ValidPrssure = 19;
-        private const int TopValidPressure = 21;
         private const int HigherPressure = 26;
 
         [Test]
@@ -23,25 +23,25 @@ namespace _10TirePressureMonitoringSystemTests
             mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(NegativePressure);
             Type alarmType = typeof(Alarm);
             Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
-            FieldInfo _sensor = alarmType.GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo sensor = alarmType.GetField("sensor", BindingFlags.Instance | BindingFlags.NonPublic);
             MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
 
-            _sensor.SetValue(classInstance, mockSensor.Object);
+            sensor.SetValue(classInstance, mockSensor.Object);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => classInstance.Check());
         }
 
         [Test]
-        public void LowerPressureActivateAlarm()
+        public void LowerPressureActivatesAlarm()
         {
             Mock<ISensor> mockSensor = new Mock<ISensor>();
             mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(LowerPressure);
             Type alarmType = typeof(Alarm);
             Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
-            FieldInfo _sensor = alarmType.GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo sensor = alarmType.GetField("sensor", BindingFlags.Instance | BindingFlags.NonPublic);
             MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
 
-            _sensor.SetValue(classInstance, mockSensor.Object);
+            sensor.SetValue(classInstance, mockSensor.Object);
 
             Assert.IsFalse(classInstance.AlarmOn);
             //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
@@ -54,79 +54,16 @@ namespace _10TirePressureMonitoringSystemTests
         }
 
         [Test]
-        public void BottomValidPressureNotActivateAlarm()
-        {
-            Mock<ISensor> mockSensor = new Mock<ISensor>();
-            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(BottomValidPressure);
-            Type alarmType = typeof(Alarm);
-            Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
-            FieldInfo _sensor = alarmType.GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic);
-            MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
-
-            _sensor.SetValue(classInstance, mockSensor.Object);
-
-            Assert.IsFalse(classInstance.AlarmOn);
-            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
-
-            checkMethod.Invoke(classInstance, null);
-
-            Assert.IsFalse(classInstance.AlarmOn);
-            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
-        }
-
-        [Test]
-        public void ValidPressureNotActivateAlarm()
-        {
-            Mock<ISensor> mockSensor = new Mock<ISensor>();
-            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(ValidPrssure);
-            Type alarmType = typeof(Alarm);
-            Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
-            FieldInfo _sensor = alarmType.GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic);
-            MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
-
-            _sensor.SetValue(classInstance, mockSensor.Object);
-
-            Assert.IsFalse(classInstance.AlarmOn);
-            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
-
-            checkMethod.Invoke(classInstance, null);
-
-            Assert.IsFalse(classInstance.AlarmOn);
-            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
-        }
-
-        [Test]
-        public void TopValidPressureNotActivateAlarm()
-        {
-            Mock<ISensor> mockSensor = new Mock<ISensor>();
-            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(TopValidPressure);
-            Type alarmType = typeof(Alarm);
-            Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
-            FieldInfo _sensor = alarmType.GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic);
-            MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
-
-            _sensor.SetValue(classInstance, mockSensor.Object);
-
-            Assert.IsFalse(classInstance.AlarmOn);
-            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
-
-            checkMethod.Invoke(classInstance, null);
-
-            Assert.IsFalse(classInstance.AlarmOn);
-            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
-        }
-
-        [Test]
-        public void HigherPressureActivateAlarm()
+        public void HigherPressureActivatesAlarm()
         {
             Mock<ISensor> mockSensor = new Mock<ISensor>();
             mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(HigherPressure);
             Type alarmType = typeof(Alarm);
             Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
-            FieldInfo _sensor = alarmType.GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo sensor = alarmType.GetField("sensor", BindingFlags.Instance | BindingFlags.NonPublic);
             MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
 
-            _sensor.SetValue(classInstance, mockSensor.Object);
+            sensor.SetValue(classInstance, mockSensor.Object);
 
             Assert.IsFalse(classInstance.AlarmOn);
             //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
@@ -135,6 +72,69 @@ namespace _10TirePressureMonitoringSystemTests
 
             Assert.IsTrue(classInstance.AlarmOn);
             //Assert.That(classInstance.AlarmOn, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void ValidPressureDoesNotActivateAlarm()
+        {
+            Mock<ISensor> mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(ValidPrssure);
+            Type alarmType = typeof(Alarm);
+            Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
+            FieldInfo sensor = alarmType.GetField("sensor", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
+
+            sensor.SetValue(classInstance, mockSensor.Object);
+
+            Assert.IsFalse(classInstance.AlarmOn);
+            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
+
+            checkMethod.Invoke(classInstance, null);
+
+            Assert.IsFalse(classInstance.AlarmOn);
+            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void BottomValidPressureDoesNotActivateAlarm()
+        {
+            Mock<ISensor> mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(BottomValidPressure);
+            Type alarmType = typeof(Alarm);
+            Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
+            FieldInfo sensor = alarmType.GetField("sensor", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
+
+            sensor.SetValue(classInstance, mockSensor.Object);
+
+            Assert.IsFalse(classInstance.AlarmOn);
+            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
+
+            checkMethod.Invoke(classInstance, null);
+
+            Assert.IsFalse(classInstance.AlarmOn);
+            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TopValidPressureDoesNotActivateAlarm()
+        {
+            Mock<ISensor> mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(TopValidPressure);
+            Type alarmType = typeof(Alarm);
+            Alarm classInstance = (Alarm)Activator.CreateInstance(alarmType);
+            FieldInfo sensor = alarmType.GetField("sensor", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo checkMethod = alarmType.GetMethod("Check", BindingFlags.Instance | BindingFlags.Public);
+
+            sensor.SetValue(classInstance, mockSensor.Object);
+
+            Assert.IsFalse(classInstance.AlarmOn);
+            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
+
+            checkMethod.Invoke(classInstance, null);
+
+            Assert.IsFalse(classInstance.AlarmOn);
+            //Assert.That(classInstance.AlarmOn, Is.EqualTo(false));
         }
     }
 }
