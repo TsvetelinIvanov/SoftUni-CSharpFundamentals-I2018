902884f6667be9a9192e129a002118fdea7d2dc3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-09 16:32:10 +0200

    Update Sensor.cs

--- CSharpOOPAdvanced/05OOPAdvancedUnitTestingExercise_/10TirePressureMonitoringSystem/Sensor.cs
+++ CSharpOOPAdvanced/05OOPAdvancedUnitTestingExercise_/10TirePressureMonitoringSystem/Sensor.cs
@@ -7,8 +7,8 @@ namespace _10TirePressureMonitoringSystem
         // The reading of the pressure value from the sensor is simulated in this implementation.
         // Because the focus of the exercise is on the other class.
 
-        const double Offset = 16;
-        private readonly Random _randomPressureSampleSimulator = new Random();
+        private const double Offset = 16;
+        private readonly Random randomPressureSampleSimulator = new Random();
 
         public double PopNextPressurePsiValue()
         {
@@ -20,7 +20,7 @@ namespace _10TirePressureMonitoringSystem
         private double ReadPressureSample()
         {
             // Simulate info read from a real sensor in a real tire
-            return 6 * this._randomPressureSampleSimulator.NextDouble() * this._randomPressureSampleSimulator.NextDouble();
+            return 6 * this.randomPressureSampleSimulator.NextDouble() * this.randomPressureSampleSimulator.NextDouble();
         }
     }
 }
