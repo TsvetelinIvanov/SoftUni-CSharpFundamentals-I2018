50159cfe48eb1fe8c4531c79da800d73da361813
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-18 14:18:09 +0200

    Update IPostViewModel.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Contracts/ViewModels/IPostViewModel.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Contracts/ViewModels/IPostViewModel.cs
@@ -2,12 +2,12 @@
 {
     public interface IPostViewModel
     {
-		string Title { get; }
+	string Title { get; }
 
-		string Author { get; }
+	string Author { get; }
 
-		string[] Content { get; }
+	string[] Content { get; }
 
-		IReplyViewModel[] Replies { get; }
+	IReplyViewModel[] Replies { get; }
     }
 }
