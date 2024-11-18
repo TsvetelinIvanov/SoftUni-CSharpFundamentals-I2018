1de3a51bd9e855d3afae77fd01de6d9b85e2ffe0
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-18 14:15:11 +0200

    Update IUserService.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Contracts/Services/IUserService.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Contracts/Services/IUserService.cs
@@ -1,15 +1,15 @@
-﻿namespace Forum.App.Contracts
-{
-	using DataModels;
+using Forum.Models;
 
+namespace Forum.App.Contracts
+{
     public interface IUserService
     {
-		bool TrySignUpUser(string username, string password);
+	bool TrySignUpUser(string username, string password);
 
-		bool TryLogInUser(string username, string password);
+	bool TryLogInUser(string username, string password);
 
-		string GetUserName(int userId);
+	string GetUserName(int userId);
 
-		User GetUserById(int userId);
+	User GetUserById(int userId);
     }
 }
