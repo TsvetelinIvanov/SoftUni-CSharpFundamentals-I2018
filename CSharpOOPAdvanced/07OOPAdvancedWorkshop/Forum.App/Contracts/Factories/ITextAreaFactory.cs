375ab7bcc1a9f156156c854127b1c026dc7278be
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-18 13:51:05 +0200

    Update ITextAreaFactory.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Contracts/Factories/ITextAreaFactory.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Contracts/Factories/ITextAreaFactory.cs
@@ -1,7 +1,7 @@
 ﻿namespace Forum.App.Contracts
 {
-	public interface ITextAreaFactory
-	{
-		ITextInputArea CreateTextArea(IForumReader reader, int x, int y, bool isPost = true);
-	}
+    public interface ITextAreaFactory
+    {
+	ITextInputArea CreateTextArea(IForumReader reader, int x, int y, bool isPost = true);
+    }
 }
