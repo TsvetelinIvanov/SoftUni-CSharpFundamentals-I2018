f43447ef1b5838a51736d7ce783b8f5b0d4f8ec7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-18 13:36:40 +0200

    Update SubmitCommand.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Commands/SubmitCommand.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Commands/SubmitCommand.cs
@@ -1,7 +1,7 @@
-﻿using Forum.App.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
+﻿using Forum.App.Contracts;
 
 namespace Forum.App.Commands
 {
@@ -28,6 +28,7 @@ namespace Forum.App.Commands
             int authorId = this.session.UserId;
 
             this.postService.AddReplyToPost(postId, replyText, authorId);
+            
             return this.session.Back();
         }
     }
