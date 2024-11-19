b87550cff59807d1740520338f2ab23f4eb658d7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-11-19 13:42:43 +0200

    Update TextAreaFactory.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Factories/TextAreaFactory.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Factories/TextAreaFactory.cs
@@ -1,33 +1,30 @@
-﻿namespace Forum.App.Factories
-{
-	using System;
-	using System.Linq;
-	using System.Reflection;
+using System;
+using System.Linq;
+using System.Reflection;
+using Forum.App.Contracts;
+using Forum.App.Models;
 
-	using Contracts;
-    using Forum.App.Models;
-
+namespace Forum.App.Factories
+{	
     public class TextAreaFactory : ITextAreaFactory
+    {
+	public ITextInputArea CreateTextArea(IForumReader reader, int x, int y, bool isPost = true)
 	{
-		public ITextInputArea CreateTextArea(IForumReader reader, int x, int y, bool isPost = true)
-		{
             //Assembly assembly = Assembly.GetExecutingAssembly();
             //Type commandType = assembly.GetTypes().FirstOrDefault(t => typeof(ITextInputArea).IsAssignableFrom(t));
-
             //if (commandType == null)
             //{
             //	throw new InvalidOperationException("TextArea not found!");
             //}
 
-            //object[] args = new object[] { reader, x, y, isPost};
-
-            //ITextInputArea commandInstance = (ITextInputArea)Activator.CreateInstance(commandType, args);
+            //object[] arguments = new object[] { reader, x, y, isPost};
+            //ITextInputArea commandInstance = (ITextInputArea)Activator.CreateInstance(commandType, arguments);
 
             //return commandInstance;
 
             ITextInputArea area = new TextInputArea(reader, x, y, isPost);
 
-			return area;
+	    return area;
         }
-	}
+    }
 }
