5ca8a4070e40fca8a2162c31396626dd94bb7eab
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-01 22:36:07 +0200

    Update MainMenu.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Menus/MainMenu.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Menus/MainMenu.cs
@@ -1,27 +1,26 @@
-﻿namespace Forum.App.Menus
-{
-	using Contracts;
-	using Models;
+using Forum.App.Contracts;
+using Forum.App.Models;
 
+namespace Forum.App.Menus
+{
     public class MainMenu : Menu
     {
-		private ISession session;
-		private ILabelFactory labelFactory;
+	private ISession session;
+	private ILabelFactory labelFactory;
         private ICommandFactory commandFactory;
 
-		public MainMenu(ISession session, ILabelFactory labelFactory, ICommandFactory commandFactory)
+	public MainMenu(ISession session, ILabelFactory labelFactory, ICommandFactory commandFactory)
         {
             this.session = session;
+	    this.labelFactory = labelFactory;
             this.commandFactory = commandFactory;
-			this.labelFactory = labelFactory;
-
+			
             this.Open();
         }
 
         protected override void InitializeButtons(Position consoleCenter)
         {
             string[] buttonContents = new string[] { "Categories", "Log In", "Sign Up" };
-
             if (session?.IsLoggedIn ?? false)
             {
                 buttonContents[1] = "Add Post";
@@ -36,21 +35,15 @@
             };
 
             this.Buttons = new IButton[buttonContents.Length];
-
             for (int i = 0; i < this.Buttons.Length; i++)
             {
                 this.Buttons[i] = labelFactory.CreateButton(buttonContents[i], buttonPositions[i]);
             }
         }
 
-		protected override void InitializeStaticLabels(Position consoleCenter)
+	protected override void InitializeStaticLabels(Position consoleCenter)
         {
-            string[] labelContents = new string[] 
-            {
-                "FORUM",
-                string.Format("Hi, {0}", this.session?.Username),
-            };
-
+            string[] labelContents = new string[] { "FORUM", string.Format("Hi, {0}", this.session?.Username) };
             Position[] labelPositions = new Position[]
             {
                 new Position(consoleCenter.Left - 4, consoleCenter.Top - 6),
@@ -58,23 +51,22 @@
             };
 
             this.Labels = new ILabel[labelContents.Length];
-
             int lastIndex = this.Labels.Length - 1;
             for (int i = 0; i < lastIndex; i++)
             {
                 this.Labels[i] = labelFactory.CreateLabel(labelContents[i], labelPositions[i]);
             }
 
-            this.Labels[lastIndex] = labelFactory
-				.CreateLabel(labelContents[lastIndex], labelPositions[lastIndex], !session?.IsLoggedIn ?? true);
+            this.Labels[lastIndex] = labelFactory.CreateLabel(labelContents[lastIndex], labelPositions[lastIndex], !session?.IsLoggedIn ?? true);
         }
 
-		public override IMenu ExecuteCommand()
-		{
+	public override IMenu ExecuteCommand()
+	{
             string commandName = string.Join("", this.CurrentOption.Text.Split()) + "Menu";
             ICommand command = commandFactory.CreateCommand(commandName);
             IMenu view = command.Execute();
+	    
             return view;
-		}
+	}
     }
 }
