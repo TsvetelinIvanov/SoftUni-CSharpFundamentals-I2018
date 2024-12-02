5441c91234ce54d5f513c21fa668da05451a63fb
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-02 20:54:08 +0200

    Update Position.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Models/Position.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Models/Position.cs
@@ -1,36 +1,36 @@
-﻿namespace Forum.App.Models
-{
-	using System;
+using System;
 
-	public struct Position
+namespace Forum.App.Models
+{
+    public struct Position
     {
         private int left;
         private int top;
 
         public Position(int left, int top)
         {
-            this.left = left;
-            this.top = top;
+            this.Left = left;
+            this.Top = top;
+        }
+	    
+	public int Left
+        {
+            get { return this.left; }
+            set { this.left = value; }
         }
 
         public int Top
         {
-            get { return top; }
-            set { top = value; }
-        }
-
-        public int Left
-        {
-            get { return left; }
-            set { left = value; }
+            get { return this.top; }
+            set { this.top = value; }
         }
 
         public static Position ConsoleCenter()
         {
             int centerTop = Console.WindowHeight / 2;
             int centerLeft = Console.WindowWidth / 2;
-
             Position center = new Position(centerLeft, centerTop);
+	
             return center;
         }
     }
