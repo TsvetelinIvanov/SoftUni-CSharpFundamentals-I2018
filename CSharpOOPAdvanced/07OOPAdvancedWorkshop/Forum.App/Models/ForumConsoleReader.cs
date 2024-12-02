6923438c0784925a43335edb0f6037457a76f6c2
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-02 20:26:38 +0200

    Update ForumConsoleReader.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Models/ForumConsoleReader.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Models/ForumConsoleReader.cs
@@ -1,47 +1,46 @@
-﻿namespace Forum.App.Models
+using System;
+using Forum.App.Contracts;
+
+namespace Forum.App.Models
 {
-	using System;
+    public class ForumConsoleReader : IForumReader
+    {
+	private bool CursorVisible { get => Console.CursorVisible; set => Console.CursorVisible = value; }
 
-	using Contracts;
-
-	public class ForumConsoleReader : IForumReader
+	public string ReadLine()
 	{
-		private bool CursorVisible { get => Console.CursorVisible; set => Console.CursorVisible = value; }
+	    int cursorLeft = Console.CursorLeft;
+	    int cursorTop = Console.CursorTop;
 
-		public string ReadLine()
-		{
-			int cursorLeft = Console.CursorLeft;
-			int cursorTop = Console.CursorTop;
-
-			return this.ReadLine(cursorLeft, cursorTop);
-		}
-
-		public string ReadLine(int cursorLeft, int cursorTop)
-		{
-			ClearRow(cursorLeft, cursorTop);
-
-			ShowCursor();
-			string result = Console.ReadLine();
-			HideCursor();
-			return result;
-		}
-
-		public void HideCursor()
-		{
-			CursorVisible = false;
-		}
-
-		public void ShowCursor()
-		{
-			CursorVisible = true;
-		}
-
-		private void ClearRow(int left, int top)
-		{
-			Console.SetCursorPosition(left, top);
-			Console.Write(new string(' ', 60 - left));
-
-			Console.SetCursorPosition(left, top);
-		}
+	    return this.ReadLine(cursorLeft, cursorTop);
 	}
+
+	public string ReadLine(int cursorLeft, int cursorTop)
+	{
+	    this.ClearRow(cursorLeft, cursorTop);
+
+	    this.ShowCursor();
+	    string result = Console.ReadLine();
+	    this.HideCursor();
+     
+	    return result;
+	}
+
+	public void HideCursor()
+	{
+	    this.CursorVisible = false;
+	}
+
+	public void ShowCursor()
+	{
+	    this.CursorVisible = true;
+	}
+
+	private void ClearRow(int left, int top)
+	{
+	    Console.SetCursorPosition(left, top);
+	    Console.Write(new string(' ', 60 - left));
+	    Console.SetCursorPosition(left, top);
+	}
+    }
 }
