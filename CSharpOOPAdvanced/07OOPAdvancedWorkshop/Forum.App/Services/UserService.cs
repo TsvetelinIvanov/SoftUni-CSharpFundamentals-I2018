a0203ccf28de7038b18ae84f557590b727e62f10
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-04 17:09:54 +0200

    Update UserService.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Services/UserService.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/Services/UserService.cs
@@ -1,10 +1,10 @@
-﻿using Forum.App.Contracts;
-using Forum.Data;
-using Forum.DataModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Forum.Data;
+using Forum.Models;
+﻿using Forum.App.Contracts;
 
 namespace Forum.App.Services
 {
@@ -20,6 +20,55 @@ namespace Forum.App.Services
         {
             this.forumData = forumData;
             this.session = session;
+        }
+
+        public bool TrySignUpUser(string username, string password)
+        {
+            bool validUsername = !string.IsNullOrWhiteSpace(username) && username.Length >= MinUsernameLength;
+            if (!validUsername)
+            {
+                throw new ArgumentException($"Username must be longer than {MinUsernameLength} symbols!");
+            }
+
+            bool validPassword = !string.IsNullOrWhiteSpace(password) && password.Length >= MinPasswordLength;
+            if (!validPassword)
+            {
+                throw new ArgumentException($"Password must be longer than {MinPasswordLength} symbols!");
+            }
+
+            bool userAlreadyExist = this.forumData.Users.Any(u => u.Username == username);
+            if (userAlreadyExist)
+            {
+                throw new InvalidOperationException("Usename taken!");
+            }
+
+            int userId = this.forumData.Users.LastOrDefault()?.Id + 1 ?? 1;
+            User user = new User(userId, username, password);            
+            this.forumData.Users.Add(user);
+            this.forumData.SaveChanges();
+            
+            this.TryLogInUser(username, password);
+            
+            return true;
+        }
+
+        public bool TryLogInUser(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            User user = this.forumData.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+            if (user == null)
+            {
+                return false;
+            }
+
+            this.session.Reset();
+            this.session.LogIn(user);
+            
+            return true;
         }
 
         public User GetUserById(int userId)
@@ -44,47 +93,6 @@ namespace Forum.App.Services
             //return user.Username;
 
             return this.GetUserById(userId).Username;
-        }
-
-        public bool TryLogInUser(string username, string password)
-        {
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-            {
-                return false;
-            }
-
-            User user = this.forumData.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
-            if (user == null)
-            {
-                return false;
-            }
-
-            this.session.Reset();
-            this.session.LogIn(user);
-            return true;
-        }
-
-        public bool TrySignUpUser(string username, string password)
-        {
-            bool validUsername = !string.IsNullOrWhiteSpace(username) && username.Length >= MinUsernameLength;
-            bool validPassword = !string.IsNullOrWhiteSpace(password) && password.Length >= MinPasswordLength;
-            if (!validPassword || !validUsername)
-            {
-                throw new ArgumentException($"Username and Password must be longer than {MinUsernameLength} symbols!");
-            }
-
-            bool userAlreadyExist = forumData.Users.Any(u => u.Username == username);
-            if (userAlreadyExist)
-            {
-                throw new InvalidOperationException("Usename taken!");
-            }
-
-            int userId = forumData.Users.LastOrDefault()?.Id + 1 ?? 1;
-            User user = new User(userId, username, password);
-            forumData.Users.Add(user);
-            forumData.SaveChanges();
-            this.TryLogInUser(username, password);
-            return true;
         }
     }
 }
