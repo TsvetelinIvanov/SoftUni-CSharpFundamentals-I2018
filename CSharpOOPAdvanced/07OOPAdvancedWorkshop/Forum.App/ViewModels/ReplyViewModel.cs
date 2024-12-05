d0232b7ddeed51a595b9e250f7d3877087c62a0f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-05 18:40:02 +0200

    Update ReplyViewModel.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/ViewModels/ReplyViewModel.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/ViewModels/ReplyViewModel.cs
@@ -1,14 +1,13 @@
-﻿using Forum.App.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
+﻿using Forum.App.Contracts;
 
 namespace Forum.App.ViewModels
 {
     public class ReplyViewModel : ContentViewModel, IReplyViewModel
     {
-        public ReplyViewModel(string author, string content)
-            : base(content)
+        public ReplyViewModel(string author, string content) : base(content)
         {
             this.Author = author;
         }
