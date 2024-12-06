0f603575ffbc351fa21d7cbd47472a6d868f1e7b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-06 22:44:09 +0200

    Update StartUp.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/StartUp.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.App/StartUp.cs
@@ -1,28 +1,25 @@
-﻿namespace Forum.App
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Forum.Data;
+using Forum.App.Contracts;
+using Forum.App.Factories;
+using Forum.App.Models;
+
+namespace Forum.App
 {
-	using System;
-
-	using Microsoft.Extensions.DependencyInjection;
-
-	using Contracts;
-	using Factories;
-    using Forum.Data;
-    using Forum.App.Models;
-
     public class StartUp
+    {
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
-		{
             IServiceProvider serviceProvider = ConfigureServices();
-			IMainController menu = serviceProvider.GetService<IMainController>();
+	    IMainController menu = serviceProvider.GetService<IMainController>();
+	    Engine engine = new Engine(menu);
+	    engine.Run();
+	}
 
-			Engine engine = new Engine(menu);
-			engine.Run();
-		}
-
-		private static IServiceProvider ConfigureServices()
-		{
-			IServiceCollection services = new ServiceCollection();
+	private static IServiceProvider ConfigureServices()
+	{
+	    IServiceCollection services = new ServiceCollection();
 
             services.AddSingleton<ITextAreaFactory, TextAreaFactory>();
             services.AddSingleton<ILabelFactory, LabelFactory>();
@@ -30,17 +27,20 @@
             services.AddSingleton<ICommandFactory, CommandFactory>();
 
             services.AddSingleton<ForumData>();
+	
             services.AddTransient<IPostService, IPostService>();
             services.AddTransient<IUserService, IUserService>();
 
             services.AddSingleton<ISession, Session>();
             services.AddSingleton<IForumViewEngine, ForumViewEngine>();
             services.AddSingleton<IMainController, IMainController>();
+            //services.AddSingleton<IMainController, MenuController>();
 
             services.AddTransient<IForumReader, ForumConsoleReader>();
 
             IServiceProvider serviceProvider = services.BuildServiceProvider();
+		
             return serviceProvider;
-		}
 	}
+    }
 }
