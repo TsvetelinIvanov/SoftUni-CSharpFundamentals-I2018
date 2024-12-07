af604e00b752f2a2868ab43226aa51112be3c934
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-07 15:37:34 +0200

    Update InvalidNumberOfScoresException.cs

--- CSharpOOPAdvanced/BashSoftOOPAdvanced/BashSoft/Exceptions/InvalidNumberOfScoresException.cs
+++ CSharpOOPAdvanced/BashSoftOOPAdvanced/BashSoft/Exceptions/InvalidNumberOfScoresException.cs
@@ -5,13 +5,13 @@ namespace BashSoft.Exceptions
     public class InvalidNumberOfScoresException : Exception
     {
         private const string InvalidNumberOfScoresExceptionMessage = "The number of scores for the given course is greater than the possible one!";
-
-        public InvalidNumberOfScoresException(string message) : base(message)
+        
+        public InvalidNumberOfScoresException() : base(InvalidNumberOfScoresExceptionMessage)
         {
 
         }
-        
-        public InvalidNumberOfScoresException() : base(InvalidNumberOfScoresExceptionMessage)
+
+        public InvalidNumberOfScoresException(string message) : base(message)
         {
 
         }
