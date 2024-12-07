9807122f276749816d44550541fb816f72ee29d2
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-07 15:38:26 +0200

    Update InvalidPathException.cs

--- CSharpOOPAdvanced/BashSoftOOPAdvanced/BashSoft/Exceptions/InvalidPathException.cs
+++ CSharpOOPAdvanced/BashSoftOOPAdvanced/BashSoft/Exceptions/InvalidPathException.cs
@@ -5,13 +5,13 @@ namespace BashSoft.Exceptions
     public class InvalidPathException : Exception
     {
         private const string InvalidPathExceptionMessage = "The source does not exist!";
-
-        public InvalidPathException(string message) : base(message)
+        
+        public InvalidPathException() : base(InvalidPathExceptionMessage)
         {
 
         }
-        
-        public InvalidPathException() : base(InvalidPathExceptionMessage)
+
+        public InvalidPathException(string message) : base(message)
         {
 
         }
