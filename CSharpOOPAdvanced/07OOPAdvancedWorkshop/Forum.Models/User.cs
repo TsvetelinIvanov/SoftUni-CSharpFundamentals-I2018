04dccd218f52fc3716c1e79da97e2baee707d92d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-07 11:54:43 +0200

    Update User.cs

--- CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.Models/User.cs
+++ CSharpOOPAdvanced/07OOPAdvancedWorkshop/Forum.Models/User.cs
@@ -1,34 +1,32 @@
-﻿namespace Forum.Models
-{
-    using System.Collections.Generic;
+using System.Collections.Generic;
 
+namespace Forum.Models
+{
     public class User
     {
-	    public User()
-	    {
+	public User()
+	{
             this.Posts = new List<int>();
-	    }
+	}
 
-	    public User(int id, string username, string password)
-            :this()
-	    {
+	public User(int id, string username, string password) : this()
+	{
             this.Id = id;
-		    this.Username = username;
-		    this.Password = password;
-	    }
+	    this.Username = username;
+	    this.Password = password;
+	}
 
-        public User(int id, string username, string password, IEnumerable<int> posts)
-            :this(id, username, password)
+        public User(int id, string username, string password, IEnumerable<int> posts) : this(id, username, password)
         {
             this.Posts.AddRange(new List<int>(posts));
         }
 
         public int Id { get; set; }
 
-		public string Username { get; set; }
+	public string Username { get; set; }
 
-		public string Password { get; set; }
+	public string Password { get; set; }
 
-	    public List<int> Posts { get; set; } = new List<int>();
+	public List<int> Posts { get; set; } = new List<int>();
     }
 }
