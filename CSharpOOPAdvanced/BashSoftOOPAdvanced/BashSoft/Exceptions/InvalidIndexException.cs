329d7e21b18ef9e5d55cb8f54dbf738176a0cbc3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-07 15:35:45 +0200

    Update InvalidIndexException.cs

--- CSharpOOPAdvanced/BashSoftOOPAdvanced/BashSoft/Exceptions/InvalidIndexException.cs
+++ CSharpOOPAdvanced/BashSoftOOPAdvanced/BashSoft/Exceptions/InvalidIndexException.cs
@@ -6,12 +6,12 @@ namespace BashSoft.Exceptions
     {
         private const string InvalidIndexExceptionMessage = "Invalid index: {0}!";
 
-        public InvalidIndexException(string message) : base(message)
+        public InvalidIndexException(int invalidIndex) : base(string.Format(InvalidIndexExceptionMessage, invalidIndex))
         {
 
         }
 
-        public InvalidIndexException(int invalidIndex) : base(string.Format(InvalidIndexExceptionMessage, invalidIndex))
+        public InvalidIndexException(string message) : base(message)
         {
 
         }
