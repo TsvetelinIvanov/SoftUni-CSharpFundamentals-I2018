4756a3a90f651b87b871a9702838bf85fd388085
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-14 20:16:35 +0200

    Update RepositorySorters.cs

--- CSharpOOPBasics/BashSoftSolution/BashSoft/Repository/RepositorySorters.cs
+++ CSharpOOPBasics/BashSoftSolution/BashSoft/Repository/RepositorySorters.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using BashSoft.IO;
+using BashSoft.StaticData;
 
-namespace BashSoft
+namespace BashSoft.Repository
 {
     public static class RepositorySorters
     {
